f924df58d3491bb8b9b9bfd61fb9ba6dbd4dfd50
Samuel Ferrer <ferrer@csb.bmc.uu.se>
2017-12-06 04:09:20 +0100

    mutation

--- popgen/mutation.py
+++ popgen/mutation.py
@@ -1,12 +1,13 @@
 import random
+import numpy
 
 def max_haplotype(pop):
 	return max([max(i) for i in pop])
 
 def mutate(pop, on = True):
 	if on:
-		for c in range(10):
-			i = random.randint(0, len(pop)-1)
+		selection = numpy.random.choice(list(range(len(pop))), size = min(len(pop), max(int(len(pop)/10),10)))
+		for i in selection:
 			a = random.randint(0, len(pop[i])-1)
 			pop[i][a] = max_haplotype(pop) + 1
 
